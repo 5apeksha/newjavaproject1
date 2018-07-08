1ae2412c34ce5313361d8746df5fd5f2826376eb
5apeksha <chavan5apeksha@gmail.com>
2018-07-08 20:18:09 +0530

    javaproject1 by apeksha

---
+++ src/Classsjava1.java
@@ -0,0 +1,13 @@
+
+public class Classsjava1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java");
+
+	}
+class Apeksha
+	{
+		
+	}
+}
